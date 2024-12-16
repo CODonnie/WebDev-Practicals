add729a0918f81a1a33ecd5c0fcf04fe6163f53c
CODonnie <ordudesigns@gmail.com>
2024-12-16 15:03:09 +0000

    note component added

--- todo-webApp/client/src/App.jsx
+++ todo-webApp/client/src/App.jsx
@@ -5,10 +5,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Todo from "./pages/todo/Todo";
 import Note from "./pages/note/Note";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const App = () => {
+
+	const url = "http://localhost:5175";
   return (
     <div className="app">
+		<ToastContainer />
       <div className="head">
         <Header />
       </div>
@@ -16,7 +22,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/todo" element={<Todo />} />
-          <Route path="/note" element={<Note />} />
+          <Route path="/note" element={<Note url={url} />} />
         </Routes>
       </div>
       <div className="foot">
--- todo-webApp/client/src/pages/note/Note.jsx
+++ todo-webApp/client/src/pages/note/Note.jsx
@@ -1,12 +1,62 @@
 //eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+import PropTypes from "prop-types";
 
-const Note = () => {
-    return (
-        <div>
-            <h1>Note Component</h1>
-        </div>
-    );
+const Note = ({ url }) => {
+  const [data, setData] = useState({
+    title: "",
+    textarea: "",
+  });
+
+  const handleData = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setData((currData) => ({ ...currData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${url}/api/note`, data);
+
+      if (response.data.success) {
+        toast.success("Note created");
+      } else {
+        toast.error("an error occured");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="note">
+      <div className="title">
+        <input
+          onChange={handleData}
+          type="text"
+          name="title"
+          value={data.title}
+          placeholder="Enter Title"
+        />
+        <textarea
+          onChange={handleData}
+          name="textarea"
+          rows="10"
+          value={data.textarea}
+          placeholder="Enter Note"
+        />
+      </div>
+      <button onClick={handleSubmit}>Save</button>
+    </div>
+  );
 };
 
 export default Note;
+
+Note.propTypes = {
+	url: PropTypes.string
+}
