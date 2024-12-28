b4949eaa51f00d8651e444dd4a060d9256a79951
CODonnie <ordudesigns@gmail.com>
2024-12-28 18:42:32 +0000

    footer and navigations reconstructed

--- todo-webApp/client/src/components/footer/Footer.jsx
+++ todo-webApp/client/src/components/footer/Footer.jsx
@@ -1,31 +1,33 @@
-//eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import "./Footer.scss";
-import { FaPlus, FaMinus, FaCheckCircle, FaBookOpen } from "react-icons/fa";
+import { FaPlus, FaMinus, FaCheck, FaBookOpen } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   const [show, setShow] = useState(false);
   return (
     <div className="footer">
-      <div className="add-icon">
-		{show ? (<FaMinus size={22} onClick={() => setShow(!show)} />)
-			: (<FaPlus size={22} onClick={() => setShow(!show)} />)
-		}
+      <div className={show ? "ani-note-icon" : "note-icon"}>
+        <Link style={{ color: "white" }} to="/note">
+          <FaBookOpen size={20} />
+        </Link>
       </div>
-      {show ? (
-        <div className="show" onClick={() => setShow(!show)}>
-          <Link className="link" to="/todo">TODO</Link>
-          <Link className="link" to="/note">NOTE</Link>
+      <div className={show ? "ani-todo-icon" : "todo-icon"}>
+        <Link style={{ color: "white" }} to="/todo">
+          <FaCheck size={20} />
+        </Link>
+      </div>
+ 
+      <div className="footer-container">
+        <div className="add-icon">
+          {show ? (
+            <FaMinus size={22} onClick={() => setShow(!show)} />
+          ) : (
+            <FaPlus size={22} onClick={() => setShow(!show)} />
+          )}
         </div>
-      ) : (
-        <></>
-      )}
-      <div className="nav">
-        <FaCheckCircle size={22} />
-        <FaBookOpen size={22} />
       </div>
-    </div>
+   </div>
   );
 };
 
