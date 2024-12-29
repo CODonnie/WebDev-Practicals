d99f5969130af22fbbc62e410fc33df3157de89c
CODonnie <ordudesigns@gmail.com>
2024-12-29 15:00:23 +0000

    footer button animation

--- todo-webApp/client/src/components/footer/Footer.jsx
+++ todo-webApp/client/src/components/footer/Footer.jsx
@@ -1,24 +1,61 @@
 import React, { useState } from "react";
 import "./Footer.scss";
-import { FaPlus, FaMinus, FaCheck, FaBookOpen } from "react-icons/fa";
+import { FaPlus, FaMinus, FaCheck, FaBookOpen, FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   const [show, setShow] = useState(false);
+  const [filter, setFilter] = useState();
+  const [selected, setSelected] = useState("home");
   return (
     <div className="footer">
       <div className={show ? "ani-note-icon" : "note-icon"}>
-        <Link style={{ color: "white" }} to="/note">
-          <FaBookOpen size={20} />
+        <Link style={{ color: "#fff" }} to="/note">
+          <FaBookOpen
+            onClick={() => {
+              setShow(!show);
+              setFilter(null);
+            }}
+            size={20}
+          />
         </Link>
       </div>
       <div className={show ? "ani-todo-icon" : "todo-icon"}>
-        <Link style={{ color: "white" }} to="/todo">
-          <FaCheck size={20} />
+        <Link style={{ color: "#fff" }} to="/todo">
+          <FaCheck
+            onClick={() => {
+              setShow(!show);
+              setFilter(null);
+            }}
+            size={20}
+          />
         </Link>
       </div>
- 
+
       <div className="footer-container">
+        <div className="home">
+          <Link style={{ color: "#fff" }} to="/home">
+            <FaHome size={30} onClick={() => {
+							setFilter("home");
+							setSelected("home");
+						}}
+					/>
+          </Link>
+          <div className="filter">
+            <div className={filter ? "ani-p1" : "p1"}
+							onClick={() => setSelected("note")}
+						>
+              <p>Note</p>
+              {selected === "note" ? <hr /> : null}
+            </div>
+            <div className={filter ? "ani-p2" : "p2"}
+							onClick={() => setSelected("todo")}
+						>
+              <p>Todo</p>
+              {selected === "todo" ? <hr /> : null}
+            </div>
+          </div>
+        </div>
         <div className="add-icon">
           {show ? (
             <FaMinus size={22} onClick={() => setShow(!show)} />
@@ -27,7 +64,7 @@ const Footer = () => {
           )}
         </div>
       </div>
-   </div>
+    </div>
   );
 };
 
